50f7467339fa1edb76c7cc5ced876809131aa63b
HeadRish <motorzx@yandex.ru>
2024-03-18 23:36:18 +0300

    testing fot_tests_bots

--- app.py
+++ app.py
@@ -1,6 +1,31 @@
 '''imports'''
-from aiogram import Bot, Dispatcher
+from aiogram import Bot, Dispatcher, F
 from aiogram.filters import CommandStart, Command
+from aiogram.types import Message
 
-bot = Bot(bot=token)
+from random import randint
 
+bot_token = "6776492979:AAH1xeTw5TN_nMy6PgvvxY2romGyVTGXxpM"
+bot = Bot(token=bot_token)
+dp = Dispatcher()
+
+count_game = 5
+user_gamer = {'in_game':False, 
+              'select_number':None,
+              'failure_game': None,
+              'total_game':0,
+              'wins_of_user_gamer':0}
+
+def get_random_number()->int:
+    return randit(1,101)
+
+@dp.message(CommandStart())
+async def ferst_start_command(message: Message):
+    await message.answer('bot for testing logarifm')
+
+@dp.message(Command(commands=['help']))
+async def starts_command(message: Message):
+    await message.answer('helps')
+
+if __name__=='__main__':
+    dp.run_polling(bot)
